230f07f6ea4c4cb698258866cd5340e283e4a617
Moesaeah King <mkc42@mail.missouri.edu>
2017-09-29 21:57:33 -0500

    Adding Files via Upload!

---
+++ app.js
@@ -0,0 +1,108 @@
+// create our angular module and inject firebase
+angular.module('scheduleApp', ['firebase'])
+
+//create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+    // connect to firebase
+    var ref = new Firebase("https://my-first-project-8e440.firebaseio.com/days");
+    var fb = $firebase(ref);
+    
+    // sync as object
+    var syncObject = fb.$asObject();
+    
+    // three way data binding
+    syncObject.$bindTo($scope, 'days');
+});
+  // function to set the default data
+  $scope.reset = function() {    
+
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0100: [
+              time: '9:00am',
+              booked: false
+          ]
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          0120: {
+              time: '12:00pm',
+              booked: false,
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0100: [
+              time: '9:00am',
+              booked: false
+          ]
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          0120: {
+              time: '12:00pm',
+              booked: false,
+          }
+        }
+      }, 
+    wednesday: {
+        name: 'Wednesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0100: [
+              time: '9:00am',
+              booked: false
+          ]
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          0120: {
+              time: '12:00pm',
+              booked: false,
+          }
+        }
+      },
+      thursday: {
+        name: 'Thursday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0100: [
+              time: '9:00am',
+              booked: false
+          ]
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          0120: {
+              time: '12:00pm',
+              booked: false,
+          }
+        }
+      }
+    }); 
+
+  };
+
+});
